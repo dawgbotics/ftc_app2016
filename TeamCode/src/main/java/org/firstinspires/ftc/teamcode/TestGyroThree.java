32c8b7a7a29c2b80b2a6632ce530dc7eff0caea1
dawgbotics <nathan.d.merrill@gmail.com>
2016-11-08 17:44:06 -0800

    Commented drive code and added PositionKnower to git

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestGyroThree.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestGyroThree.java
@@ -47,8 +47,8 @@ public class TestGyroThree extends OpMode {
 
     @Override
     public void loop() {
-        ourGyro.read();
-        telemetry.addData("z", ourGyro.getAngle());
+        ourGyro.readZ();
+        telemetry.addData("z", ourGyro.getAngleZ());
     }
 
     @Override
