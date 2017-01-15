2a9df94251c78f1b5d8ce7d23d415fb1c657ae4c
Bren11 <awsomeduckman@gmail.com>
2017-01-14 23:11:44 -0800

    comments

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HardwareTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HardwareTest.java
@@ -1,35 +1,3 @@
-/*
-Copyright (c) 2016 Robert Atkinson
-
-All rights reserved.
-
-Redistribution and use in source and binary forms, with or without modification,
-are permitted (subject to the limitations in the disclaimer below) provided that
-the following conditions are met:
-
-Redistributions of source code must retain the above copyright notice, this list
-of conditions and the following disclaimer.
-
-Redistributions in binary form must reproduce the above copyright notice, this
-list of conditions and the following disclaimer in the documentation and/or
-other materials provided with the distribution.
-
-Neither the name of Robert Atkinson nor the names of his contributors may be used to
-endorse or promote products derived from this software without specific prior
-written permission.
-
-NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
-LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-"AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
-THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESSFOR A PARTICULAR PURPOSE
-ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
-FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
-DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
-SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR
-TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
-THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-*/
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.*;
@@ -37,21 +5,12 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
-/**
- * This file contains an minimal example of a Linear "OpMode". An OpMode is a 'program' that runs in either
- * the autonomous or the teleop period of an FTC match. The names of OpModes appear on the menu
- * of the FTC Driver Station. When an selection is made from the menu, the corresponding OpMode
- * class is instantiated on the Robot Controller and executed.
- *
- * This particular OpMode just executes a basic Tank Drive Teleop for a PushBot
- * It includes all the skeletal structure that all linear OpModes contain.
- *
- * Use Android Studios to Copy this Class, and Paste it into your team's code folder with a new name.
- * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
- */
-
 @com.qualcomm.robotcore.eventloop.opmode.Autonomous(name="hardware test", group="Linear Opmode")  // @Autonomous(...) is the other common choice
 public class HardwareTest extends LinearOpMode {
+
+    //this program tests each system, with the motors and gyroscope going autonomously and
+    //telling the operator via telemetry if they work. The rest goes until the operator
+    //hits a button
 
     /* Declare OpMode members. */
     private ElapsedTime runtime = new ElapsedTime();
@@ -72,14 +31,13 @@ public class HardwareTest extends LinearOpMode {
     @Override
     public void runOpMode() {
 
-        drive = new Drive(hardwareMap, "gyro", telemetry);
-        drive.resetEncoders();
-
+        //creates a gyro object
         Gyro gyro = new Gyro(hardwareMap, "gyro");
 
+        //creates all motors and servos
         motorGun1 = hardwareMap.dcMotor.get("gun 1");
         motorGun1.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        motorGun1.setDirection(DcMotorSimple.Direction.REVERSE);
+        motorGun1.setDirection(DcMotorSimple.Direction.FORWARD);
 
         motorIntake = hardwareMap.dcMotor.get("intake");
         motorIntake.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
@@ -107,6 +65,7 @@ public class HardwareTest extends LinearOpMode {
         motorRightBack.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         motorRightFront.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
+        //runs each motor and tells you if the encoder works
         motorLeftBack.setPower(0.618);
         sleep(500);
         motorLeftBack.setPower(0);
@@ -144,10 +103,9 @@ public class HardwareTest extends LinearOpMode {
             telemetry.addData("RightFront: ", "bad");
         }
 
-
+        sleep(3000);
 
         waitForStart();
-        runtime.reset();
 
         //start gun test
         motorGun1.setPower(.5);
